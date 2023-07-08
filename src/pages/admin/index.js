1b0a643b6069249eee6053cbc889781a24b8ebdd
MYEONG JOON <junvictory@gmail.com>
2023-07-08 21:23:33 +0900

    feat : Admin Table Page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import { MaterialReactTable } from 'material-react-table'
+import axios from 'axios'
+
+const fetchProduct = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/product')
+    return response
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 const index = () => {
-  return <div></div>
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetchProduct().then((result) => {
+      setData(result.data.product)
+      //   console.log(result.data)
+    })
+  }, [])
+  console.log(data)
+  const columns = useMemo(
+    () => [
+      {
+        header: 'ID',
+        accessorKey: 'PR_ID_PK', //simple accessorKey pointing to flat data
+      },
+      {
+        header: 'Name',
+        accessorKey: 'PR_NAME', //simple accessorKey pointing to flat data
+      },
+      {
+        header: 'AlC',
+        accessorKey: 'PR_ALC', //simple accessorKey pointing to flat data
+      },
+    ],
+    []
+  )
+  return <MaterialReactTable columns={columns} data={data} />
 }
 
 export default index
