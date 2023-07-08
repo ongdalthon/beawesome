09f9e86c9069374ee06b42c382d8c13d06297edc
wooseok123 <hosmimam@naver.com>
2023-07-08 21:44:55 +0900

    feat : detail page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     styledComponents: true,
   },
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
