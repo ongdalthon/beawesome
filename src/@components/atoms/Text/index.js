d09e055d9f4210dd159def8a43fdba56922625f1
wooseok123 <hosmimam@naver.com>
2023-07-08 14:53:41 +0900

    feat : make components of atomic design pattern

---
+++ src/@components/atoms/Text/index.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const Text = ({
+  size = 1,
+  weight = 400,
+  color = 'Black',
+  spacing = 0,
+  font = 'Pretendard',
+  height = 120,
+  cursor = 'auto',
+  background,
+  gradient = false,
+  align = 'start',
+  text,
+  ...rest
+}) => {
+  return (
+    <StyledText
+      size={size}
+      weight={weight}
+      color={color}
+      spacing={spacing}
+      font={font}
+      height={height}
+      cursor={cursor}
+      align={align}
+      {...rest}
+    >
+      {text}
+    </StyledText>
+  )
+}
+
+const StyledText = styled.span`
+  font-size: ${({ size }) => size}px;
+  font-weight: ${({ weight }) => weight};
+  color: ${({ color }) => color};
+  letter-spacing: ${({ spacing }) => spacing}px;
+  line-height: ${({ height }) => height}%;
+  font-family: ${({ font }) => font};
+  text-align: ${({ align }) => align};
+  cursor: ${({ cursor }) => cursor};
+  background: ${({ background }) => background};
+  ${({ gradient }) =>
+    gradient
+      ? `-webkit-background-clip: text;
+  -webkit-text-fill-color:transparent;
+  background-clip: text;
+  text-fill-color:transparent;
+
+  `
+      : null};
+`
